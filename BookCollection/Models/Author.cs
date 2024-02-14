4e948c74c4c9fe4f1dda30c5e0661ebf3e5edc01
SOTGGON <maoxiehui@outlook.com>
2024-02-14 18:12:39 +0100

    Andra layout och ny model till författare

---
+++ BookCollection/Models/Author.cs
@@ -0,0 +1,9 @@
+namespace BookCollection.Models {
+    public class Author  {
+        // Properties
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int NumberOfBooks { get; set; }
+    }
+}
